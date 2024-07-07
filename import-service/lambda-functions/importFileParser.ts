acbe94653ae0dbb374f21008c8f1c1d1319f5c7a
Rustam Satlykov <rustam.satlykov@proton.me>
2024-07-08 00:45:37 +0500

    fix: fixes for catalogBatchProcess

--- import-service/lambda-functions/importFileParser.ts
+++ import-service/lambda-functions/importFileParser.ts
@@ -9,69 +9,84 @@ import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
 import type { S3Handler } from "aws-lambda";
 import * as csv from "csv-parser";
 
-const BUCKET_REGION = "eu-central-1";
-const QUEUE_URL = '';
+const BUCKET_REGION =  process.env.REGION ?? "eu-central-1";
+const SQS_URL = process.env.SQS_URL;
 const s3Client = new S3Client({ region: BUCKET_REGION });
 const sqsClient = new SQSClient({ region: BUCKET_REGION });
 
+const sendSQSMessage = async (data: Record<string, string>) => {
+    let result = null;
+    try {
+        const messageBody = JSON.stringify(data);
+        result = await sqsClient.send(
+            new SendMessageCommand({
+                QueueUrl: SQS_URL,
+                MessageBody: messageBody,
+            }),
+        );
+        console.log("Message sent to SQS:", messageBody);
+    } catch (e) {
+        console.error("Error sending message to SQS:", e);
+    }
+    return result;
+};
+
 export const handler: S3Handler = async (event) => {
     console.log("importFileParser event", event);
-    console.log('Queue URL:', QUEUE_URL);
 
-    for (const record of event.Records) {
-        const { bucket, object } = record.s3;
+    try {
+        await Promise.all(
+            event.Records.map(async (record) => {
+                const { bucket, object } = record.s3;
 
-        try {
-            const { Body } = await s3Client.send(
-                new GetObjectCommand({
-                    Bucket: bucket.name,
-                    Key: object.key,
-                }),
-            );
-            const readableStream = Body as Readable;
+                const { Body } = await s3Client.send(
+                    new GetObjectCommand({
+                        Bucket: bucket.name,
+                        Key: object.key,
+                    }),
+                );
+                const readableStream = Body as Readable;
 
-            readableStream
-                .pipe(csv())
-                .on("data", console.log)
-                .on("end", async () => {
-                    console.log(`CSV file ${object.key} processed`);
+                const sqsPromises: Promise<unknown>[] = [];
 
-                    const sendMessageCommand = new SendMessageCommand({
-                        QueueUrl: QUEUE_URL,
-                        MessageBody: JSON.stringify(record)
-                    });
-
-                    try {
-                        await sqsClient.send(sendMessageCommand);
-                        console.log('Record sent to SQS:', record);
-                    } catch (e) {
-                        console.error('Error sending message to SQS:', e);
-                        throw e;
-                    }
-
-                    const destinationKey = object.key.replace("uploaded/", "parsed/");
-                    await s3Client.send(
-                        new CopyObjectCommand({
-                            Bucket: bucket.name,
-                            CopySource: `${bucket.name}/${object.key}`,
-                            Key: destinationKey,
-                        }),
-                    );
-                    console.log(`File copied to ${destinationKey}`);
-
-                    await s3Client.send(
-                        new DeleteObjectCommand({
-                            Bucket: bucket.name,
-                            Key: object.key,
-                        }),
-                    );
-                    console.log(`File deleted from ${object.key}`);
-                })
-                .on("error", (e) => {
-                    console.error("Error processing file:", e);
+                await new Promise((resolve, reject) => {
+                    readableStream
+                        .pipe(csv())
+                        .on("data", async (data) => {
+                            sqsPromises.push(sendSQSMessage(data));
+                        })
+                        .on("end", async () => {
+                            await Promise.all(sqsPromises);
+                            console.log(`CSV file ${object.key} processed`);
+                            resolve(true);
+                        })
+                        .on("error", (e) => {
+                            console.error("Error processing file:", e);
+                            reject(e);
+                        });
                 });
-        } catch (e) {
-            console.error("Error processing file:", e);
-        }
+
+                const destinationKey = object.key.replace("uploaded/", "parsed/");
+
+                await s3Client.send(
+                    new CopyObjectCommand({
+                        Bucket: bucket.name,
+                        CopySource: `${bucket.name}/${object.key}`,
+                        Key: destinationKey,
+                    }),
+                );
+                console.log(`File copied to ${destinationKey}`);
+
+                await s3Client.send(
+                    new DeleteObjectCommand({
+                        Bucket: bucket.name,
+                        Key: object.key,
+                    }),
+                );
+                console.log(`File deleted from ${object.key}`);
+            }),
+        );
+    } catch (e) {
+        console.error("Error processing file:", e);
     }
 };
--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -7,28 +7,67 @@ import {
     aws_apigateway,
     aws_lambda,
     aws_lambda_event_sources,
-    aws_s3, aws_sqs,
+    aws_s3, aws_sqs, aws_sns_subscriptions, aws_sns, aws_dynamodb,
 } from "aws-cdk-lib";
 import type { Construct } from "constructs";
 
 const BUCKET_NAME = process.env.name ??  "import-service-s3-satlykov-rustam";
+const PRODUCT_TABLE_NAME = 'products';
+const STOCK_TABLE_NAME =  'stocks';
 
 export class ImportServiceStack extends Stack {
-    public readonly catalogItemsQueueArn: string;
 
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        const catalogItemsQueue = new aws_sqs.Queue(this, 'catalogItemsQueue', {
-            visibilityTimeout:  Duration.seconds(300),
-            receiveMessageWaitTime: Duration.seconds(20)
-        })
+        const productsTable = aws_dynamodb.Table.fromTableName(
+            this,
+            `${PRODUCT_TABLE_NAME}Ref`,
+            PRODUCT_TABLE_NAME,
+        );
 
-        this.catalogItemsQueueArn = catalogItemsQueue.queueArn;
-        new cdk.CfnOutput(this, 'CatalogItemsQueueArnOutput', {
-            value: this.catalogItemsQueueArn,
-            exportName: 'CatalogItemsQueueArn',
-        });
+        const stocksTable = aws_dynamodb.Table.fromTableName(
+            this,
+            `${STOCK_TABLE_NAME}Ref`,
+            STOCK_TABLE_NAME,
+        );
+
+        const catalogItemsQueue = new aws_sqs.Queue(
+            this,
+            "CatalogItemsQueue",
+            {
+                queueName: "CatalogItemsQueue"
+            },
+        );
+        const sqsEventSource = new aws_lambda_event_sources.SqsEventSource(
+            catalogItemsQueue,
+            {
+                batchSize: 5,
+            },
+        );
+
+        const createProductTopic = new aws_sns.Topic(
+            this,
+            "CreateProductSNSTopic",
+            {
+                topicName: "CreateProductSNSTopic",
+            },
+        );
+        createProductTopic.addSubscription(
+            new aws_sns_subscriptions.EmailSubscription('satlikov.rustam.mail@gmail.com'),
+        );
+        createProductTopic.addSubscription(
+            new aws_sns_subscriptions.EmailSubscription(
+                'rustam.satlykov@proton.me',
+                {
+                    filterPolicy: {
+                        price: aws_sns.SubscriptionFilter.numericFilter({
+                            greaterThanOrEqualTo: 300,
+                        }),
+                    },
+                },
+            ),
+        );
 
         const bucket = new aws_s3.Bucket(
             this,
@@ -58,7 +97,7 @@ export class ImportServiceStack extends Stack {
 
         const importProductsFileLambda = new aws_lambda.Function(
             this,
-            "ImportProductsFileBucketSatlykov",
+            "ImportProductsFileLambda",
             {
                 functionName: "ImportProductsFileLambda",
                 runtime: aws_lambda.Runtime.NODEJS_18_X,
@@ -94,15 +133,15 @@ export class ImportServiceStack extends Stack {
 
         const importFileParserLambda = new aws_lambda.Function(
             this,
-            "ImportFileParser",
+            "ImportFileParserLambda",
             {
                 functionName: "ImportFileParserLambda",
                 runtime: aws_lambda.Runtime.NODEJS_18_X,
                 handler: "importFileParser.handler",
                 code: aws_lambda.Code.fromAsset("lambda-functions"),
                 environment: {
-                    BUCKET_REGION: this.region,
-                    CATALOG_ITEMS_QUEUE_URL: catalogItemsQueue.queueUrl,
+                    REGION: this.region,
+                    SQS_URL: catalogItemsQueue.queueUrl,
                 },
             },
         );
@@ -115,5 +154,26 @@ export class ImportServiceStack extends Stack {
             filters: [{ prefix: "uploaded/" }],
         });
         importFileParserLambda.addEventSource(s3EventSource);
+
+        const catalogBatchProcessFunction = new aws_lambda.Function(
+            this,
+            'CatalogBatchProcessHandler',
+            {
+                functionName: "CatalogBatchProcessHandler",
+                runtime: aws_lambda.Runtime.NODEJS_18_X,
+                code: aws_lambda.Code.fromAsset("lambda-functions"),
+                handler: "catalogBatchProcess.handler",
+                environment: {
+                    REGION: this.region,
+                    TOPIC_ARN: createProductTopic.topicArn,
+                    STOCKS_TABLE_NAME: stocksTable.tableName,
+                    PRODUCT_TABLE_NAME: productsTable.tableName,
+                },
+            }
+        )
+        productsTable.grantWriteData(catalogBatchProcessFunction);
+        stocksTable.grantWriteData(catalogBatchProcessFunction);
+        createProductTopic.grantPublish(catalogBatchProcessFunction);
+        catalogBatchProcessFunction.addEventSource(sqsEventSource);
     }
 }
